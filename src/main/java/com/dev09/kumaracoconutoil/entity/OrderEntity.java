1e7eedba38245af4d8559c3d59e9de6fb1b72e89
Dinuka Lakmal <Dinuka0512@gmail.com>
2026-03-22 17:14:10 +0530

    Created the 2 UIs .. Login and the dashboard

---
+++ src/main/java/com/dev09/kumaracoconutoil/entity/OrderEntity.java
@@ -0,0 +1,11 @@
+package com.dev09.kumaracoconutoil.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class OrderEntity {
+    @Id
+    private String OrderId;
+
+}
---
+++ src/main/java/com/dev09/kumaracoconutoil/entity/ProductEntity.java
@@ -0,0 +1,4 @@
+package com.dev09.kumaracoconutoil.entity;
+
+public class ProductEntity {
+}
